9ec855faf7763499b5a4305578b6aa2cdd9a5dea
Kiko Sprague <ksprague0592@gmail.com>
2024-01-22 06:14:11 +0000

    first attempt to use put to read a string of characters to std output

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * A program that prints some text with the put function.
+ */
+int main()
+{
+	char myput[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(myput);
+
+	return 0;
+}
