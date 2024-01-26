0e9bfcf75b5963bf2e109f80db8a26c26a86f9e7
kiko sprague <ksprague92@gmail.com>
2024-01-26 06:22:35 +0000

    using putchar to spell putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/** main - entry point
+ *
+ * return: 0
+ */
+
+int main()
+{
+	int word;
+
+	word = '_';
+	putchar(word);
+	word = 'p';
+	putchar(word);
+	word = 'u';
+	putchar(word);
+	word = 't';
+	putchar(word);
+	word = 'c';
+	putchar(word);
+	word = 'h';
+	putchar(word);
+	word = 'a';
+	putchar(word);
+	word = 'r';
+	putchar(word);
+	putchar('\n');
+	return(0);
+}
+
