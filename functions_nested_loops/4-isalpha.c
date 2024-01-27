c05afd06aa732c9110c6b691cc1b22f1ababaa70
kiko sprague <ksprague92@gmail.com>
2024-01-27 22:32:10 +0000

    return 1 if a letter and 0 if anything else

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha- checks if a char is a letter aA - zZ
+ * @a:
+ *
+ * return: 1 if true and 0 if false
+ */
+
+int _isalpha(int a);
+{
+	if (a <= 97 && a >= 122 || a <= 65 && a >= 90);
+
+	{
+		return (1);
+
+	}
+
+	return(0)
+
+}
