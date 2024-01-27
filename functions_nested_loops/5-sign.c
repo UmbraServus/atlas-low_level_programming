6459588dc1c039d43ae695f2507d795db5135eab
kiko sprague <Ksprague92@gmail.com>
2024-01-27 23:55:41 +0000

    if and else if nests to print the signs of n

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign- entry point printing the sign of n +, -, or 0
+ * @n: the character to be checked
+ *
+ * Return: 1,0, or -1
+int print_sign(int n);
+{
+	
+	if (n > 0)
+	{
+
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar(n);
+			return (0);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-');
+		return(-1)
+	}
+}
