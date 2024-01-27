ba671224f35295cdefe481a24f63e117398aac85
kiko sprague <ksprague92@gmail.com>
2024-01-27 22:36:07 +0000

    didnt realize () had to be between the two logical && statements

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -8,13 +8,13 @@
 
 int _isalpha(int a)
 {
-	if (a <= 97 && a >= 122 || a <= 65 && a >= 90);
+	if (a <= 97 && a >= 122) || (a <= 65 && a >= 90);
 
 	{
 		return (1);
 
 	}
 
-	return(0)
+	return(0);
 
 }
