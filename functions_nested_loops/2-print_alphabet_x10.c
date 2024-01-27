eccdc367a51daa666eb668f53c472e645e77716a
kiko sprague <ksprague92@gmail.com>
2024-01-27 20:38:39 +0000

    first attempt at printing alphabet x10 using nested for loop inside of while

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int a = 0;
+	char i;
+
+	while(a<10)
+	{
+		for(i = a; i <= z; i++);
+		{
+		_putchar(i);
+		}
+
+		_putchar('\n');
+	}
+}
