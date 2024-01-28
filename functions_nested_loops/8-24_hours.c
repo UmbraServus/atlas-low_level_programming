8ec403066106d2e0f2290c02c2219febc4f25ea2
kiko sprague <ksprague92@gmail.com>
2024-01-28 07:09:31 +0000

    wrong letters on variables

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 {
 	int a, b;
 	
-	for (a = 0; a < 24; h++)
+	for (a = 0; a < 24; a++)
 	{
-	for (b = 0; b < 60; m++)
+	for (b = 0; b < 60; b++)
 	{
 		_putchar((a / 10) + '0');
 		_putchar((a % 10) + '0');
