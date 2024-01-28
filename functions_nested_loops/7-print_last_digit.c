050c21f6b5f99b5c6bf0b5351a6484e7d6bc9b67
kiko sprague <ksprague92@gmail.com>
2024-01-28 06:28:21 +0000

    idk

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,18 +7,18 @@
 
 int print_last_digit(int a)
 {
+	int b = a % 10;
 
-	if (a > 0)
+	if (b > 0)
 	{
-		int b = a % 10;
 
 		_putchar(b);
 	}
-	else if (a < 0)
+	else if (b < 0)
 	{
-	      int c = (a % 10) * -1;
+		b = b * -1;
 
-	       _putchar(c);
+	       _putchar(b);
 	}
-return (0);
+return (b);
 }
