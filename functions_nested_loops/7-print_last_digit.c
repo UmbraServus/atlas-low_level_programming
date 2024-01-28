98c222fb9cb01cf2d7a760fb11850fc13e1f4124
kiko sprague <ksprague92@gmail.com>
2024-01-28 06:15:30 +0000

    using while loops to print value of a's last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+
+int print_last_digit(int a)
+{
+
+	while (a > 0)
+	{
+		int a = a % 10;
+
+		_putchar(a);
+	}
+	while (a < 0)
+	{
+	       int a = (a % 10) * -1;
+
+	       _putchar(a);
+	}
+return (0);	
