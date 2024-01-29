236ebe5d920c9a4dddf982aea5a0ecbe70395cf3
kiko sprague <ksprague92@gmail.com>
2024-01-29 06:32:12 +0000

    checks for a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit- checks to see if its a number or a letter
+ * @: number to be checked
+ *
+ * Return: 1 if a digit 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57);
+	{
+		return (1);
+	}
+
+	return (0);
+}
