b54fbbd471a4e7bcf89d16b1da55162d7eb3c56e
kiko sprague <ksprague92@gmail.com>
2024-01-30 04:15:04 +0000

    prints 0 - 14 ten times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers- putchars 0 - 14 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void);
+{
+	int i;
+	i = 0
+	while (i < 10)
+	{
+		if (i < 14)
+		{
+			_putchar((i / 10)+ '0');
+			_putchar((i % 10)+ '0');
+		}
+		
+		_putchar("\n");
+	}
+	i++
+}
