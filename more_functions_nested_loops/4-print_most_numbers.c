6092e1aae0c141e98db5a18bbc7e5ff76db749de
kiko sprague <ksprague92@gmail.com>
2024-01-30 03:26:40 +0000

    print 0 - 9 omit the 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers- prints numbers 0 - 9 omitting 2 and 4
+ * @i: number string to be fed to putchar
+ *
+ * Return- void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		if (i == 2 || i == 2)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
