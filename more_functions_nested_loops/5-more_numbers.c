7dbe0dce725ff0a0069a2942a775ee8709b88203
kiko sprague <ksprague92@gmail.com>
2024-01-30 04:46:16 +0000

    betty code style fix

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,22 +7,21 @@
 void more_numbers(void)
 {
 	int a, i;
+
 	a = 0;
 
-	while (a < 10 )
+	while (a < 10)
 	{
-		for (i = 0;i <= 14; i++)
+		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
 			{
-			_putchar((i / 10)+ '0');
+			_putchar((i / 10) + '0');
 			}
 
-			_putchar((i % 10)+ '0');
+			_putchar((i % 10) + '0');
 		}
-		
 		_putchar('\n');
-		
 		a++;
 	}
 
