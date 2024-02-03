45f13bffda6fb0ff6aa094819226c7e45570d91b
kiko sprague <ksprague92@gmail.com>
2024-02-03 17:40:18 +0000

    first attempt to create puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+
+void _puts(char *str)
+{
+	int x;
+
+	x = 0;
+
+	while (str[x] != '\0')
+	{
+		_putchar(str[x]);
+			
+			x++;
+	}
+	_putchar('\n')
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,4 +22,5 @@ void print_diagonal(int n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 #endif
