e2d3e03b32417d0eeb35ad4e6705a9c717153391
kiko sprague <ksprague92@gmail.com>
2024-02-03 19:25:52 +0000

    whitespace

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,17 +6,16 @@
  * Return: void
  *
  */
-
 void print_rev(char *s)
 {
 	int x, len;
-	
+
 	len = 0;
 		while (s[len] != '\0')
 		{
 			len++;
 		}
-	
+
 	for(x = len - 1; x >= 0; x--)
 	{	
 		_putchar(s[x]);
