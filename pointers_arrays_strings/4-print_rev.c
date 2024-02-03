b4c6c71822d9c2a1361d74b833e74a88d0fbe7a8
kiko sprague <ksprague92@gmail.com>
2024-02-03 19:13:49 +0000

    changed back to how it was a few commits ago

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,14 @@ void print_rev(char *s)
 	int x, len;
 	
 	len = 0;
-		while (*s[len] != '\0')
+		while (s[len] != '\0')
 		{
 			len++;
 		}
 	
 	for(x = len - 1; x >= 0; x--)
 	{	
-		_putchar(*s[x]);
+		_putchar(s[x]);
 	}
 	_putchar('\n');
 }
