ae021f40215dd3286fea84b71e3362bd16b7d60b
kiko sprague <ksprague92@gmail.com>
2024-02-03 21:50:51 +0000

    forgot inlcude main.h

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,13 @@
+#include "main.h"
+
 void puts2(char *str)
 
 {
 	int x;
 
-	for (x = 0; s[x] != '\0'; x += 2)
+	for (x = 0; str[x] != '\0'; x += 2)
 	{
-		_putchar(s[x]);
+		_putchar(str[x]);
 	}
 	_putchar('\n');
 }
