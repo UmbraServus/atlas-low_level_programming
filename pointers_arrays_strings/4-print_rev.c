82bd0dc4d04e6f12415ac3d68552bbfde9ef5b01
kiko sprague <ksprague92@gmail.com>
2024-02-03 18:13:51 +0000

    first attempt at rev string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+
+void print_rev(char *s);
+{
+	int length;
+	
+	for (length = _strlen(s); length = 0; length--)
+	{	
+		_putchar(s[length]);
+	}
+	_putchar('\n')
+}
