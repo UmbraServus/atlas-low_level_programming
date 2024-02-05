3122e058eb0bbf8c6682623e4c0f001334659cc5
kiko sprague <ksprague92@gmail.com>
2024-02-05 16:17:48 +0000

    while loop to minus s2 from s1.

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#inlcude "main.h"
+/**
+ * _strcmp- compare strings
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ *
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return *s1 - *s2;
+		}
+
+		s1++;
+		s2++;
+	
+	}
+
+	return *s1 - *s2
+}
