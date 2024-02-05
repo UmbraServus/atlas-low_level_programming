4be24d8a2a78f6781fb2cf2748cb7033e7763399
kiko sprague <ksprague92@gmail.com>
2024-02-05 16:25:26 +0000

    betty doc

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *
  * _strncpy- copy src striing into des string
  * @dest: string to be copied over
  * @src: source string to be copied
- * @n- the number of characters to be copied
+ * @n: the number of characters to be copied
  *
  * Return: dest
+ *
  */
 
 char *_strncpy(char *dest, char *src, int n)
