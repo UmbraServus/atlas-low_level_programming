a96aafa5d711198de453df4d879c4aed14f837f1
kiko sprague <ksprague92@gmail.com>
2024-02-06 05:29:20 +0000

    idk

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *str)
 
 		for ( n = 0; str[n]; n++)
 		{
-			if (str[n] == 0 || str[n] == ' ' || str[n] == '\t' || str[n] == '\n' || str[n] == ',' || str[n] == ';' || str[n] == '.' || str[n] == '!' || str[n] == '?' || str[n] == '"' || str[n] == '(' || str[n] == ')' || str[n] == '{' || str[n] == '}')
+			if (str[n] >= 'A' && str[n] <= 'Z' || str[n] == 0 || str[n] == ' ' || str[n] == '\t' || str[n] == '\n' || str[n] == ',' || str[n] == ';' || str[n] == '.' || str[n] == '!' || str[n] == '?' || str[n] == '"' || str[n] == '(' || str[n] == ')' || str[n] == '{' || str[n] == '}')
 			{
 				word = 0;
 			}
@@ -24,11 +24,6 @@ char *cap_string(char *str)
 			{
 				str[n] = str[n] - 32;
 				word = 1;
-			}
-			
-			else if (word && str[n] >= 'A' && str[n] <= 'Z')
-			{
-			
 			}
 		}
 	return (str);
