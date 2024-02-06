0ecda3886c68b7ef8581c68e39b0de34fb7ee275
kiko sprague <ksprague92@gmail.com>
2024-02-06 03:07:42 +0000

    change to uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * string_toupper- changes a string to an uppercase from lowercase
+ * @str: string to be checked and changed to upper from lower case
+ *
+ * Return: str
+ *
+ */
+
+char *string_toupper(char *str);
+{
+	int n;
+
+	for (n = 0; str[n]; n++)
+	{
+		if (str[n] >= 97 && str[n] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+
+	}
+	
+	return str;
+}
