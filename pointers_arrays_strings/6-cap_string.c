e38367216f049eeacaa139fb8da4e64309ee89a0
kiko sprague <ksprague92@gmail.com>
2024-02-06 05:11:51 +0000

    took out the word = 0 on the last block

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -20,14 +20,13 @@ char *cap_string(char *str)
 				word = 0;
 			}
 
-			else if (!word && str[n] >=97 && str[n] <= 122)
+			else if (!word && str[n] >= 97 && str[n] <= 122)
 			{
 				str[n] = str[n] - 32;
 				word = 1;
 			}
 			else if (word && str[n] >= 65 && str[n] <= 90)
 			{
-				word = 0;
 
 			}
 		}
