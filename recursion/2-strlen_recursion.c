0e09112352b88f5776da8c21396e6915a661ab0c
kiko sprague <ksprague0592@gmail.com>
2024-02-09 18:52:57 +0000

    str len using recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion- counts string length recursively
+ * @s: char string to count 
+ *
+ * Return: int or length of string
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+
+		return(0);
+
+	}
+
+	return(_strlen_recursion(s + 1) + 1);
