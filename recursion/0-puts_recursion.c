b5198970b4bf4349f15e31a4cd7531a07200e6eb
kiko sprague <ksprague0592@gmail.com>
2024-02-09 18:07:57 +0000

    different method altogether

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -9,15 +9,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s = '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);
+		_putchar('\n')
 
-		_puts_recursion(s + 1);
+			return;
 	}
-	else (*s = '\0')
-	{
-	_putchar('\0');
-	}	
-
+	_putchar(*s)
+		s++
+		_puts_recursion(s);
 }
