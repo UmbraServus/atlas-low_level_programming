6fbf6ae65057883e5cc3c1c278b91f8d5ad9da3a
kiko sprague <ksprague92@gmail.com>
2024-02-10 23:47:32 +0000

    betty

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
+ * _sqrt_recursion- sqrt recursively
+ * @n: number to find the sqrt of
  *
- *
- *
+ * Return: natural sqrt, -1 or 1
  *
  */
 
@@ -16,8 +17,17 @@ int _sqrt_recursion(int n)
 
 		return (-1);
 
-	return(sqrt_helper(n, 1));
+	return (sqrt_helper(n, 1));
 }
+
+/**
+ * sqrt_helper: helper function to do the sqrt recursively
+ * @n: number to find sqrt of
+ * @m: the numbers to iterate thru until found a natural square of n
+ *
+ * Return: -1 if negative and natural sqrt
+ *
+ */
 
 int sqrt_helper(int n, int m)
 {
