fc250de049adb9845b00ecae822fe7cf35ab0ad9
kiko sprague <ksprague92@gmail.com>
2024-02-10 22:18:41 +0000

    idk

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+int _sqrt_recursion(int n)
+{
+	int m;
+
+	m = n / 2
+
+	if (m * m > n)
+	return (_sqrt_recursion(int n - ( m - 1) * (m - 1)));
+
+	else if (m * m == n)
+	{
+		return (m);
+	}
+	else if ( m < 0)
+	{
+		return (-1);
+	}
+}
