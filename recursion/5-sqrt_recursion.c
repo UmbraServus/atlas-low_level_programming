c07ab6f3fb5f4851f316e664ecbfbe40a4e75b78
kiko sprague <ksprague92@gmail.com>
2024-02-10 23:25:00 +0000

    im not sure

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -13,10 +13,14 @@ int sqrt_helper(int n, int m);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
+
 		return (-1);
-	}
-	
+
+	else if (n == 1 || n == 0)
+
+		return (n);
+	else
+
 	return(sqrt_helper(n, n / 2));
 }
 
@@ -33,6 +37,5 @@ int sqrt_helper(int n, int m)
 	else
 
 	return (sqrt_helper(n, m - 1));
-
 
 }
