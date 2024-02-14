f87a4f20ca2c8cd00eaa00856f2a3624d125c8cb
kiko sprague <ksprague0592@gmail.com>
2024-02-14 17:21:09 +0000

    malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char new_c;
+	int i;
+
+	new_c = malloc(sizeof(char) * (size + 1));
+
+	if (size = 0 || c = '\0')
+	{
+		return (null);
+	}
+
+	while (i < size)
+	{
+		new_c[i] = c;
+			i++;
+	}
+	new_c[i] = '\0';
+	return (new_c);
+}
