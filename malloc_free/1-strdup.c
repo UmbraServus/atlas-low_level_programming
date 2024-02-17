a28a3e28b5c683834c40a8be27b79d49953a77f4
kiko sprague <ksprague92@gmail.com>
2024-02-17 22:41:12 +0000

    first try at string dupe

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,5 +2,49 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-char *_strdup(char *str)
+/**
+ * _strlen- calculates length of string
+ * @s: string to be calculated
+ *
+ * Return: x
+ *
+ */
 
+int _strlen(char *s)
+{
+
+	int x;
+
+	x = 0;
+
+		while (s[x] != '\0')
+		{
+			x++;
+		}
+	return (x);
+}
+
+char *_strdup(char *str)
+{
+	char *new_str;
+	size_t i;
+	int length;
+
+	i = 0;
+	length = _strlen(*str);
+	new_str = malloc(sizeof(char) * (length + 1));
+
+	if (str == NULL || new_str = NULL)
+	{
+		return (NULL);
+	}
+
+	while (i < length)
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	new_str[i] = '\0';
+
+	return (new_str);
+}
