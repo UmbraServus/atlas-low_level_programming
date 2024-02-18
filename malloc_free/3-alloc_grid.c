b383d3873e2b84b5f5f3e7fbd56616870cf09ec6
kiko sprague <ksprague92@gmail.com>
2024-02-18 02:31:03 +0000

    first attempt at grid alloc

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int column;
+	int row;
+
+	if (width <= 0 || height <= 0)
+	{
+		return  (NULL);
+	}
+	
+	grid = malloc(sizeof(int) * height);
+		
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (column = 0; column < height; column++)
+	{
+		grid[column] = malloc(sizeof(int) * width);
+
+		if (grid[column] == NULL)
+		{
+			for (row = 0; row < column; row++)
+			{
+				free(grid[row]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (row = 0; row < width; row++)
+		{
+			grid[column][row] = 0;
+		}
+	}
+	
+	return (grid);
+}
