e19a66193db7fbce879bd69d0c842d1c5dfcaf17
kiko sprague <ksprague0592@gmail.com>
2024-02-20 04:41:05 +0000

    first attempt at malloc check

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked- checks if malloc worked and exits with a 98 value if failed
+ * @b: the amount of memory to be allocated
+ *
+ * Return: mallcheck if successful
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *mallcheck;
+		
+		mallcheck = malloc(b);
+		
+		if (mallcheck == NULL)
+		{
+			exit(98);
+		}
+
+		return (mallcheck);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+0- checks if malloc worked and exits if failed returns an address if successful
