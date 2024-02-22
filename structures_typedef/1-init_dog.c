2993951913d4b02db428a4e20d1be1e3ce20da0a
kiko sprague <ksprague92@gmail.com>
2024-02-22 18:06:57 +0000

    took out malloc and other stuff

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -13,14 +13,6 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *d
-
-		d = malloc(sizeof(struct dog));
-
-	if (d == NULL)
-	{
-		return (NULL);
-	}
 	
 	d->name = name;
 	d->age = age;
