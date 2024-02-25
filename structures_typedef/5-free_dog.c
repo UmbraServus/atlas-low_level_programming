6d0634791a39006766937bda326eb7e336c69742
kiko sprague <ksprague92@gmail.com>
2024-02-25 05:06:12 +0000

    first attempt to free

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog- frees dog struct and elements
+ * @d: pointer to struct
+ *
+ * Return: void
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (dog == NULL)
+	{
+		exit(98);
+	}
+
+	free(d->name);
+	free(d->owner);
+	free(d)
+}
