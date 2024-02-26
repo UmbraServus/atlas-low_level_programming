ae0b155a25ad2ba89c2ed2b816ba7e7da0d8ee93
kiko Sprague <ksprague92@gmail.com>
2024-02-26 05:36:36 +0000

    first attempt 0-printname

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_name- prints names using function pointer to other functions
+ * @name: name to be printed
+ * @f: function to be used.
+ *
+ * Return: void
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+
+	f(name);
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,1 @@
+0-print_name - prints name using function pointer passed.
