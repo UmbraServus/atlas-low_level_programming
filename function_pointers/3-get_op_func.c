74c7b86448a8b43d50804289dbd0b4c1b0a35253
kiko sprague <ksprague92@gmail.com>
2024-02-27 05:31:34 +0000

    get op function

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,1 +1,34 @@
 #include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_op_func- gets a function based on which operator is input
+ * @s: operator
+ *
+ * Return: gives answer to the operator function
+ *
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	   op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+
+    while (ops[i].op != NULL)
+    {
+	    if (ops[i].op == *s)
+	    {
+		    return (ops[i].f);
+	    }
+	    i++
+    }
+    return (NULL);
+}
