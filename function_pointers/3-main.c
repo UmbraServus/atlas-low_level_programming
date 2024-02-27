787142174951f11eefc871987a8d5d3fd5888835
kiko sprague <ksprague92@gmail.com>
2024-02-27 05:47:49 +0000

    idk

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,22 +1,18 @@
 #include "3-calc.h"
 #include <stdio.h>
-#include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * main- the main function
- * @argc: number of args
- * @argv: arr of args
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
  *
- * Return: err or 0
+ * Return: Always 0
  */
-
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char op;
-	char *valid_ops;
+	int num1, num2, result;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
@@ -26,21 +22,22 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	op = argv[2][0];
-	valid_ops = "+-*/%";
+	operation = get_op_func(argv[2]);
 
-	if ((strchr(valid_ops, op) == NULL) || (strlen(argv[2]) != 1))
+	if (operation == NULL)
 	{
 		printf("Error\n");
 		return (99);
 	}
 
-	if ((op == '/' || op == '%') && (num1 == 0 || num2 == 0))
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		return (100);
 	}
 
-	printf("%d\n", get_op_func(&op)(num1, num2));
+	result = operation(num1, num2);
+	printf("%d\n", result);
+
 	return (0);
 }
