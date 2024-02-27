8b5cc340c5f8eec06a848127b27f154b7d6c4034
kiko sprague <ksprague92@gmail.com>
2024-02-27 22:07:17 +0000

    changed my operator iterator to be a pointer and added ; on the increment of i

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -25,11 +25,11 @@ int (*get_op_func(char *s))(int, int)
 
     while (ops[i].op != NULL)
     {
-	    if (ops[i].op == *s && *(s + 1) == '\0')
+	    if (ops[i].op[0] == *s && *(s + 1) == '\0')
 	    {
 		    return (ops[i].f);
 	    }
-	    i++
+	    i++;
     }
     return (NULL);
 }
