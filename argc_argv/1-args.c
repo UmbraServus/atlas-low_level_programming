732474419339454c26db8e4507ce0092ab54c42a
kiko sprague <ksprague92@gmail.com>
2024-02-28 05:20:25 +0000

    first attmept argc argv -1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main- prints the count of args
+ * @argc: argument count
+ * @argv: arg vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[0])
+{
+	printf(%d, argc - 1);
+
+	void(argv);
+
+	return (0)
+}
