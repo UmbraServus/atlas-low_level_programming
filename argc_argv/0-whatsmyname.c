4828917f8b1754048eb039499f39087cb108ea69
kiko sprague <ksprague92@gmail.com>
2024-02-28 05:05:53 +0000

    accidentally pasted some comment from another code

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -10,19 +10,6 @@
  *
  */
 
-#include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * print_numbers- prints n amount of numbers seprated by a separator if
- * not null
- * @
- *
- *
- */
-
 void main (int argc, char *argv[]);
 {
 	printf("%s\n", argv[0])
