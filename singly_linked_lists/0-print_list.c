b3fbe4e5a223388740da29bd19551a5bee63aa9f
umbraservus <ksprague0592@gmail.com>
2024-03-07 00:13:53 +0000

    fixed some betty spaces, and whitespaces, and what? anothe space betweeen return and the parenthesis

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,12 +13,13 @@ size_t print_list(const list_t *h)
 {
 	int Count;
 	const list_t *pointer = NULL;
+
 	pointer = h;
 	Count = 0;
 
 	if (h == NULL)
 	{
-		return(0);
+		return (0);
 	}
 
 
@@ -30,7 +31,7 @@ size_t print_list(const list_t *h)
 			pointer = pointer->next;
 			Count++;
 		}
-		
+
 		else
 		{
 			printf("[%d] %s\n", pointer->len,  pointer->str);
