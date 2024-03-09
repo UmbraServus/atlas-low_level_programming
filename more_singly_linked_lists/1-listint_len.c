9589845683fbf845a2230d169cfa691815fce54f
kiko sprague <ksprague92@gmail.com>
2024-03-09 17:50:21 +0000

    1- lists number of  elements in the linked lists

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * listint_len- counts the number of elements in the list.
+ * @h the head of the list
+ *
+ *
+ * Return: counter of elements
+ *
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *current;
+	size_t counter;
+
+	current = h;
+	counter = 0;
+
+	if (h == NULL)
+	{
+		return (counter);
+	}
+
+	while (current != NULL)
+	{
+		counter++;
+		current = current->next;
+	}
+
+	return (counter);
+}
--- more_singly_linked_lists/README.md
+++ more_singly_linked_lists/README.md
@@ -1,1 +1,2 @@
 0-print all ints and count elements.
+1-counts the elements in the list and returns count
