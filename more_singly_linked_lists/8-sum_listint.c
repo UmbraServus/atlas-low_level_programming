869ae8f492f6461a5e480785dd547307c1d0ceb7
kiko sprague <ksprague92@gmail.com>
2024-03-10 00:37:28 +0000

    sums the int elements in the list

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_listint- adds all the int elements up in the list
+ * @head: head reference
+ *
+ *
+ * Return: int sum of all numbers
+ *
+ */
+
+int sum_listint(listint_t *head)
+{
+	int n;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	n = 0;
+
+	while (head != NULL)
+	{ 
+		 n = n + head->n;
+		 head = head->next;
+	}
+
+	return (n);
+}
