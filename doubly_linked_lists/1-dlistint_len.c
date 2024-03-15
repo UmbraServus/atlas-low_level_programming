0141a83d48a937bd205f078034b567530f04d688
kiko sprgue <ksprague0592@gmail.com>
2024-03-15 17:59:53 +0000

    counts elements doubly linked list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * dlistint_len counts elements thru a doubly linked list
+ * @h: starting point in the doubly linked list
+ *
+ * Return: counter
+ *
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t counter;
+	const dlistint_t *current;
+
+	counter = 0;
+	current = h;
+
+	if (h == NULL)
+	{
+		return (counter);
+	}
+
+	while (current != NULL)
+	{
+
+		current = current->next;
+		counter++;
+	}
+
+	return (counter);
+}
