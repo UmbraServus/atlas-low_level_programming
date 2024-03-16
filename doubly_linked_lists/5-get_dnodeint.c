c7608361c9020773763907c1f76ee53af7f50550
kiko sprague <ksprague0592@gmail.com>
2024-03-16 22:34:04 +0000

    5- traverse list to nth index and return that index

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index- gets node at the nth index and returns that node
+ * @head: start of linked list
+ * @index: number of nodes to traverse
+ *
+ *
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int c;
+
+	current = head;
+
+	for (c = 0; c < index; c++)
+	{
+		if (head == NULL)
+		{
+			return(NULL);
+		}
+
+		current = current->next;
+	}
+
+	return (current);
+}
--- doubly_linked_lists/README.md
+++ doubly_linked_lists/README.md
@@ -2,3 +2,8 @@
 1- counts elements of linked list
 2- add node to head of list
 3- add node to end of list
+4- frees the linked list
+5- go to nth index of the linked list and return that node
+6-
+7-
+8-
