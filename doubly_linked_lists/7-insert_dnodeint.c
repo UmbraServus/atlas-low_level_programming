b5aaba47c9432ea9617cccd3dddab4d003297ea4
kiko sprague <ksprague0592@gmail.com>
2024-03-17 00:10:20 +0000

    insert new node at index -7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index- inserts new node at the nth index
+ * @h: head of linked list
+ * @idx: the nth index to traverse to
+ * @n: int data for the new node
+ *
+ * Return: NULL on failure, new on success
+ *
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current;
+	dlistint_t *new;
+	unsigned int i;
+
+	current = *h;
+
+	if (*h == NULL)
+	{
+		return (NULL);
+	}
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->prev = NULL;
+	new->n = n;
+	new->next = NULL;
+
+	for (i = 0; i < idx; i++)
+	{
+		current = current->next;
+	}
+
+	new->prev = current->prev;
+	new->next = current;
+	current->prev->next = new;
+	current->prev = new;
+
+	return (new);
+}
