b6707879681040dccc162965d680842fca2436ee
kiko sprague <ksprague0592@gmail.com>
2024-03-17 01:16:06 +0000

    added a check after for loop to see if current is NULL

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -35,6 +35,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 	}
 
+	if (current == NULL)
+	{
+		return (NULL);
+	}
+
 	new = malloc(sizeof(dlistint_t));
 	
 	if (new == NULL)
