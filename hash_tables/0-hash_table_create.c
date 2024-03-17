18870af74afaee3f7e35ddff831e069906a50487
kiko sprague <ksprague0592@gmail.com>
2024-03-17 05:22:19 +0000

    forgot to add malloc checks and initialize ht->size to size

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -18,7 +18,21 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 
 	ht = malloc(sizeof(hash_table_t));
+
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+
 	ht->array = malloc(sizeof(hash_node_t*) * size);
+
+	if (ht->array == NULL)
+	{
+		free(ht->array);
+		return (NULL);
+	}
+
+	ht->size = size;
 
 	for(i = 0; i < size; ++i)
 	{
