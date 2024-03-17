29c8ef4e99751c4bc425ccbe2a12a79f3f2cec94
kiko sprague <ksprague0592@gmail.com>
2024-03-17 01:24:59 +0000

    -1 in for loop

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	}
 
-	for (i = 0; i < idx; i++)
+	for (i = 0; i < idx - 1; i++)
 	{
 		if (current->next == NULL)
 		{
