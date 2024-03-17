01287c32a47537b2eb737f172656703213f29911
kiko sprague <ksprague0592@gmail.com>
2024-03-17 01:01:09 +0000

    trying to fix where if it is an empty list it adds node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (*h == NULL || idx == 0)
 	{
-		return (NULL);
+		return (add_dnodeint(h, n));
 	}
 
 	for (i = 0; i < idx; i++)
