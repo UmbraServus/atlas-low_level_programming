77135e6f8b43ec9d2e5a7e93d1a4751b63b2dd89
kiko sprague <ksprague0592@gmail.com>
2024-03-17 00:58:26 +0000

    took out the add node functions

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -27,16 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	for (i = 0; i < idx; i++)
 	{
-		if (current->next == NULL)
-		{
-			return (add_dnodeint_end(h, n));
-		}
-
-		if (current->prev == NULL)
-		{
-			return (add_dnodeint(h,n));
-		}
-
+	
 		current = current->next;
 	}
 
