919ee3c34cd17d944097b7d08f14f07a2d36fd0e
kiko sprague <ksprague0592@gmail.com>
2024-03-17 05:10:06 +0000

    create an empty hashtable nth long

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create- creates an empty hash table of nth size
+ * @size: nth size of hashtable
+ *
+ *
+ * Return: hashtable
+ *
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	ht->array = malloc(sizeof(hash_node_t*) * size);
+
+	for(i = 0; i < size; ++i)
+	{
+		ht->array[i] = NULL;
+	}
+
+	return (ht);
+}
