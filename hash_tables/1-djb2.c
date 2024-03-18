8cb7d2ad58c13cdd24c9cc422f521fdf33a2c927
kiko sprague <ksprague92@gmail.com>
2024-03-18 04:42:00 +0000

    after hours of google and talking to ai I think I have an idea of how this works

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,12 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_djb2- a function to turn a str n2 an int for indexing in a hash table
+ * @str: str to be converted to a hash using an algorithm
+ *
+ *
+ *
+ */ 
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
-
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	unsigned long int hash;
+	int c;
+	
+	hash = 5381;
+	
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	
+	return (hash);
 }
---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * key_index- uses hash algorithm to turn key into hash n assign indx
+ * @key: str to be converted to hash
+ * @size: size of the array of ptrs to keys
+ * 
+ * Return: index
+ *
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+	unsigned long int i;
+	unsigned long int index;
+
+	for (i = 0; i < size; i++)
+	{
+		hash = hash_djb2(key);
+	}
+
+	index = hash % size;
+
+	return (index);
+}
