9b1a3aa591bc0ba1d1b3b7d10140e2d82e399986
kiko sprague <ksprague0592@gmail.com>
2024-03-19 03:46:31 +0000

    idk

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *prev;
 	hash_node_t *array_index = ht->array[index];
-	hash_node_t *new_node;
+
 
 	if (array_index == NULL)
 	{
