6c3bf896d63fddf8c0a7dd1796481492a9d8fbdf
kiko sprague <ksprague0592@gmail.com>
2024-03-19 04:06:07 +0000

    finally now just to get malloc return to work right

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -67,6 +67,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
+	if (new_node->key || new_node->value == NULL)
+	{
+		free(new_node);
+		return (-1);
+	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
