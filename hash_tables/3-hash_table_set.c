0be7c723aef9ef11ede6e6e70955998bb8fca509
kiko sprague <ksprague0592@gmail.com>
2024-03-19 04:19:39 +0000

    idk how i broke it but i did jeeze

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	hash_node_t *array_index = ht->array[index];
 
-	if (ht || key || value == NULL)
+	if (ht || key == NULL)
 	{
 		return (-1);
 	}
