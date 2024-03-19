e7b638d776620512245b34375351f9a6525ac907
kiko sprague <ksprague0592@gmail.com>
2024-03-19 02:39:14 +0000

    changed some strcpy to dup instead and added a line to make ht = array_index

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -26,33 +26,31 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (-1);
 		}
 
-		array_index->key = malloc(sizeof(key) + 1);
+		array_index->key = strdup(key);
 		if (array_index->key == NULL)
 		{
 			free(array_index);
-			return (-1);
+			return (-1)
 		}
 
-		array_index->value = malloc(sizeof(value) + 1);
-		if (array_index->value == NULL)
+		array_index->value = strdup(value);
+		if (array_index->value = NULL)
 		{
 			free(array_index);
-			return (-1);
+			return (-1)
 		}
 
-		strcpy(array_index->key, key);
-		strcpy(array_index->value, value);
-
 		array_index->next = NULL;
+		ht->array[index] = array_index
 
 		return (1);
 	}
 
 	while (array_index != NULL)
 	{
-		if (strcmp(array_index->key,key) == 0)
+		if (strcmp(array_index->key, key) == 0)
 		{
-			free (array_index->value);
+			free(array_index->value);
 			array_index->value = strdup(value);
 			return (1);
 		}
