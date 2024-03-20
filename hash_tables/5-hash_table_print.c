dd122be6c066b9ea8e8690b5a5365c47bc25e78c
kiko sprague <ksprague92@gmaiul.com>
2024-03-20 05:29:20 +0000

    trailing white spaces and documentation

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,36 +1,38 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_print- prints the hashtable
+ * @ht: ht to be printed
  *
- *
- *
+ * Return: void just print
  *
  */
 
 
- void hash_table_print(const hash_table_t *ht)
+void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *node;
 	int comma;
+
 	if (ht == NULL)
 	{
 		printf("{}");
 	}
-	
-	
+
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
-		
+
 		while (node != NULL)
 		{
 			if (comma == 1)
 			{
 				printf(", ");
 			}
-			
+
 			printf("'%s': '%s'", node->key, node->value);
 			comma = 1;
 			node = node->next;
