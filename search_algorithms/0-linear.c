c8b5f5b27d6f1174db4591f87166b92206ef8328
kiko sprague <ksprague0592@gmail.com>
2024-04-10 15:15:35 +0000

    linear search with a for loop simple return i on success and -1 on fail

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * linear_search- searches through an array one at a time.
+ * @array: array to be searched through
+ * @size: size of the array(how many numbers are in it).
+ * @value: number to look for in the array
+ *
+ * Return: -1 on fail and index of array on success
+ *
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+	return (-1);
+	}
+	for(i = 0; i < size; i++)
+	{
+		printf("Value checked array[i] = [%d]\n", array[i]);
+
+		if (array[i] == value)
+		{
+			return(i);
+		}
+	}
+
+return(-1);
+}
